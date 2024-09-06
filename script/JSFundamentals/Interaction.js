0e295cea33320ff8824e17a6e8af7d0fa9376c00
Fernando Nerio Jr <fernandonerio90@gmail.com>
2024-09-05 23:45:21 -0600

    feat: add interaction js file for practise

---
+++ script/JSFundamentals/Interaction.js
@@ -0,0 +1,7 @@
+// Interaction: Alerts, Prompts and Confirms
+
+// Interaction #1: Alerts
+const heroButton = document.querySelector('.hero-button');
+heroButton.addEventListener('click', function () {
+  alert("Welcome back to coding!!")
+});
