164399065a031215846d36301660ce3859526285
Fernando Nerio Jr <fernandonerio90@gmail.com>
2024-09-07 21:30:06 -0600

    feat: add alerts, prompt and confirm

--- script/JSFundamentals/Interaction.js
+++ script/JSFundamentals/Interaction.js
@@ -1,7 +1,24 @@
 // Interaction: Alerts, Prompts and Confirms
 
-// Interaction #1: Alerts
+// Alerts and Prompt combine
+
+
+/*
+  Here I'm using an interactive button where I use an event listener
+  to activate a prompt question whenever the hero button is click, 
+  then it alerts the user of how old they are.
+*/
+/*
+  The function prompt accepts two functions:
+  Title and the prompt as shown below
+  "prompt(title, [...])"
+*/
 const heroButton = document.querySelector('.hero-button');
-heroButton.addEventListener('click', function () {
-  alert("Welcome back to coding!!")
+heroButton.addEventListener('click', function() {
+
+  let name = prompt('What is your name?', []);
+
+  alert(`Hi ${name}, welcome back to coding!`)
 });
+//  Last is confirm
+// "confirm("Press "Okay" if you are ready to code.")"
